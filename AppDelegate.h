28a8bcb63961ec0810f5dcd3ecaeb54f90e2daca
iosDevelopmentTeam <sabyasachi@gstcloud.com>
2015-11-05 11:56:21 +0530

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  MedicalMates
+//
+//  Created by User on 03/02/15.
+//  Copyright (c) 2015 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ReferApatientVC;
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ReferApatientVC *viewController;
+
+@end
+
