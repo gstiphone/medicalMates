b855d1430d0e7311aa1a540110cb2a6e60e421bd
iosDevelopmentTeam <sabyasachi@gstcloud.com>
2015-11-05 12:54:46 +0530

    Create CustomAppointmentListCell.m

---
+++ CustomAppointmentListCell.m
@@ -0,0 +1,23 @@
+//
+//  CustomAppointmentListCell.m
+//  MedicalMates
+//
+//  Created by User on 09/02/15.
+//  Copyright (c) 2015 User. All rights reserved.
+//
+
+#import "CustomAppointmentListCell.h"
+
+@implementation CustomAppointmentListCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
