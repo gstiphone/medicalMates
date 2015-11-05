dc97b8f60d231e8e20f19b755dfc667a5be1384b
iosDevelopmentTeam <sabyasachi@gstcloud.com>
2015-11-05 13:03:17 +0530

    Create CustomAppointmentHistory.m

---
+++ CustomAppointmentHistory.m
@@ -0,0 +1,23 @@
+//
+//  CustomAppointmentHistory.m
+//  MedicalMates
+//
+//  Created by User on 18/02/15.
+//  Copyright (c) 2015 User. All rights reserved.
+//
+
+#import "CustomAppointmentHistory.h"
+
+@implementation CustomAppointmentHistory
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
